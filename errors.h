e2a75c414dc01de760b160a57a57df001df58e92
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2025-08-24 17:51:00 -0500

    comments for error conditions

--- errors.h
+++ errors.h
@@ -2,24 +2,26 @@
 
 enum class Error
 {
-    NONE = 0,
-    SUCCESS = 0,
-    NULLPTR = -1,
-    NO_DEVICE = -2,
-    BAD_PARAM = -3,
-    INVALID = -4,
-    UNINITIALIZED = -5,
-    BUSY = -6,
-    BAD_STATE = -7,
-    UNKNOWN = -8,
-    COMMS = -9,
-    TIMEOUT = -10,
-    NO_RESPONSE = -11,
-    OVERFLOW = -12,
-    UNDERFLOW = -13,
-    NONE_AVAIL = -14,
-    SHUTDOWN = -15,
-    ABORT = -16,
-    NOT_SUPPORTED = -17,
-    FAIL = -255
+    NONE = 0,      /* No error, operation successful */
+    NULLPTR,       /* nullptr when not expected */
+    NO_DEVICE,     /* Device does not exist (ex: UART4 requested when only has 3)*/
+    BAD_PARAM,     /* Parameter does not make sense */
+    INVALID,       /* Logical invalid cant be enabled and off*/
+    UNINITIALIZED, /* Use of uninitialized resource */
+    BUSY,          /* Resource is busy and cannot complete operation */
+    BAD_STATE,     /* Operation attempted in wrong state or state should not have reached */
+    UNKNOWN,       /* Cannot pinpoint error (likely asssert) */
+    COMMS,         /* Failed to communicate */
+    TIMEOUT,       /* Timeoout waiting for operation */
+    NO_RESPONSE,   /* No response from resource */
+    OVERFLOW,      /* Overflow of buffer or other container */
+    UNDERFLOW,     /* Attempt to read from a buffer with nothing */
+    NONE_AVAIL,    /* No resource available */
+    SHUTDOWN,      /* Error shutting down */
+    ABORT,         /* Operation aborted */
+    NOT_SUPPORTED, /* Operation not supported */
+    LOCKED,        /* Resource locked by other process or accidentally */
+    PERMISSIONS,   /* Operation not permitted (ex: override non overridable data)*/
+    IO_FAIL,       /* Generic IO failure (ex: UART framing error, parity mismatch)*/
+    CRC,           /* CRC Mismatch */
 };
